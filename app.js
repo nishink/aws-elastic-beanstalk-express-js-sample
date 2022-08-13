95c15a6570ff8d8ca08b8ec073cd2e4d8b949781
nishink <nishink@hotmail.com>
2022-08-13 16:53:11 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Potato, Shiromo, Abbey, Choco and Teddy!'));
+app.get('/', (req, res) => res.send('Hello Molcars! Potato, Shiromo, Abbey, Choco and Teddy!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
